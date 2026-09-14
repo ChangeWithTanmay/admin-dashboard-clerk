d1dc46703b5abfcefa59d4519f146eaeee20f677
ChangeWithTanmay <tanmaydas24pgs@gmail.com>
2026-09-15 02:23:02 +0530

    Add login page component with form and styling

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      loginPage
+    <div className='w-full h-screen flex items-center justify-center'>
+      <form action="" className='bg-bgSoft p-8 rounded-[10px] w-100 h-100 flex flex-col justify-center gap-7.5 items-center'>
+        <h1 className='font-bold text-2xl text-center'>Login Page</h1>
+        <input 
+        className='p-4 border-2 border-[#2e374a] rounded-[5px] bg-bg text-text w-full' 
+        type="text" 
+        placeholder='username' />
+        <input 
+        className='p-4 border-2 border-[#2e374a] rounded-[5px] bg-bg text-text w-full' 
+        type="text" 
+        placeholder='password' />
+
+        <button className='p-4 bg-[teal] text-text border-none cursor-pointer rounded-[5px] w-full'>Login</button>
+      </form>
     </div>
   )
 }
